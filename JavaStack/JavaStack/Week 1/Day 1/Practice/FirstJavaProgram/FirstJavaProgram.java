a8e653e67c66d28094caeb5191bd1fa8e83e653b
Aouichi-Achref <aouichi123achref@gmail.com>
2024-05-14 15:39:45 +0200

    commit

---
+++ JavaStack/JavaStack/Week 1/Day 1/Practice/FirstJavaProgram/FirstJavaProgram.java
@@ -0,0 +1,11 @@
+//! bech tebda 
+public class FirstJavaProgram{
+    // ! your main can be many
+    public static void main(String[] args){
+        // ! System.out.println() bech taamel console.log()
+        System.out.println("My name is L'aouichi Achref");
+        System.out.println("My age is 26");
+        System.out.println("My hometown is Soukra DarFadhal");
+        System.out.println("My team is Club Africain");
+    }
+}
