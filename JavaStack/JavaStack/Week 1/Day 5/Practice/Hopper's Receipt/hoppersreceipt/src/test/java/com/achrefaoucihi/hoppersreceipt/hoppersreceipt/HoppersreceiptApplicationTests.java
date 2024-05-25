966e0ba816fa32501fceea07a7b5e7624718e43f
Aouichi-Achref <aouichi123achref@gmail.com>
2024-05-25 16:30:10 +0200

    commit

---
+++ JavaStack/JavaStack/Week 1/Day 5/Practice/Hopper's Receipt/hoppersreceipt/src/test/java/com/achrefaoucihi/hoppersreceipt/hoppersreceipt/HoppersreceiptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.achrefaoucihi.hoppersreceipt.hoppersreceipt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoppersreceiptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
