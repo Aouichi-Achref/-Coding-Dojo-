a7c6af97c923094aebda9962d0e479da125b52a8
Aouichi-Achref <aouichi123achref@gmail.com>
2024-06-04 11:52:01 +0200

    commit

---
+++ JavaStack/JavaStack/Week 3/Day 1/Core/Log_Reg/demo/src/main/java/com/achref/logreg/Controller/UserController.java
@@ -0,0 +1,88 @@
+package com.achref.logreg.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.achref.logreg.Entity.LoginUser;
+import com.achref.logreg.Entity.User;
+import com.achref.logreg.Service.UserServ;
+
+import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
+
+@Controller
+public class UserController {
+
+    // Add once service is implemented:
+     @Autowired
+     private UserServ userServ;
+    
+    @GetMapping("/")
+    public String index(Model model) {
+    
+        // Bind empty User and LoginUser objects to the JSP
+        // to capture the form input
+        model.addAttribute("newUser", new User());
+        model.addAttribute("newLogin", new LoginUser());
+        return "Index.jsp";
+    }
+    
+    @PostMapping("/register")
+    public String register(@Valid @ModelAttribute("newUser") User newUser, 
+            BindingResult result, Model model, HttpSession session) {
+        
+        // TO-DO Later -- call a register method in the service 
+        // to do some extra validations and create a new user!
+        userServ.register(newUser, result);
+        if(result.hasErrors()) {
+            // Be sure to send in the empty LoginUser before 
+            // re-rendering the page.
+            model.addAttribute("newLogin", new LoginUser());
+            return "Index.jsp";
+        }
+        
+        // No errors! 
+        // TO-DO Later: Store their ID from the DB in session, 
+        // in other words, log them in.
+        session.setAttribute("user_id", newUser.getId());
+        return "redirect:/Welcome";
+    }
+    
+    @PostMapping("/login")
+    public String login(@Valid @ModelAttribute("newLogin") LoginUser newLogin, 
+            BindingResult result, Model model, HttpSession session) {
+        
+        // Add once service is implemented:
+         User user = userServ.login(newLogin, result);
+    
+        if(result.hasErrors()) {
+            model.addAttribute("newUser", new User());
+            return "Index.jsp";
+        }
+    
+        // No errors! 
+        // TO-DO Later: Store their ID from the DB in session, 
+        // in other words, log them in.
+        session.setAttribute("user_id", user.getId());
+        return "redirect:/Welcome";
+    }
+    
+    @GetMapping("/Welcome")
+    public String welcome(HttpSession session,Model model) {
+    	Long user_id=(Long)session.getAttribute("user_id");
+    	model.addAttribute("name",userServ.findUserById(user_id).getUserName());
+    	return "Welcome.jsp";
+    }
+    
+    @GetMapping("/logout")
+    public String logout(HttpSession s) {
+    	s.invalidate();
+    	return "redirect:/";
+    }
+    
+}
